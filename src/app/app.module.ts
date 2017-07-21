e4a9498ca54b26456772a56ce7201975a49a9a6e
Justin Gomez <programjustin@gmail.com>
2017-07-21 09:29:07 -0700

    add imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { EditGeocacheComponent } from './edit-geocache/edit-geocache.component';
 import { GeocacheDetailComponent } from './geocache-detail/geocache-detail.component';
 import { GeocacheListComponent } from './geocache-list/geocache-list.component';
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 @NgModule({
   declarations: [
@@ -21,7 +34,12 @@ import { GeocacheListComponent } from './geocache-list/geocache-list.component';
     GeocacheListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
